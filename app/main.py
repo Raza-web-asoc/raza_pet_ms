1dccd375ba313487f4ee0511991073fe254bcb4a
Jonathan David <jonathan.jnthnd@gmail.com>
2024-12-17 00:45:19 -0500

    database conection

--- app/main.py
+++ app/main.py
@@ -1,23 +1,15 @@
-from fastapi import FastAPI
-from sqlalchemy import create_engine, text
+import sys
 import os
+from fastapi import FastAPI
+
+# Añadir el directorio raíz del proyecto al PYTHONPATH
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from app.db.base import init_db
 
 app = FastAPI()
 
-# Configuración de la base de datos
-DATABASE_USER = os.getenv("DATABASE_USER", "root")
-DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD", "rootpassword")
-DATABASE_HOST = os.getenv("DATABASE_HOST", "localhost")
-DATABASE_PORT = os.getenv("DATABASE_PORT", "3306")
-DATABASE_NAME = os.getenv("DATABASE_NAME", "app_db")
-
-DATABASE_URL = (
-    f"mysql+pymysql://{DATABASE_USER}:{DATABASE_PASSWORD}"
-    f"@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
-)
-
-# Conexión con la base de datos
-engine = create_engine(DATABASE_URL)
+init_db()
 
 @app.get("/")
 def read_root():
