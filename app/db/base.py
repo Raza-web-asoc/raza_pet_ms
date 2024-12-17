c42509ec27329b4d00f3876720f441623eae0d86
Jonathan David <jonathan.jnthnd@gmail.com>
2024-12-17 01:21:05 -0500

    data base conection healtly

--- app/db/base.py
+++ app/db/base.py
@@ -1,29 +1,15 @@
-import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from dotenv import load_dotenv
+from app.core.config import settings
 
-load_dotenv()
-
-DB_NAME = os.getenv('DB_NAME')
-DB_USER = os.getenv('DB_USER')
-DB_PASSWORD = os.getenv('DB_PASSWORD')
-DB_HOST = os.getenv('DB_HOST')
-DB_PORT = os.getenv('DB_PORT')
-
-# Validar que todas las variables de entorno estén definidas
-if not all([DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT]):
-    raise ValueError("Una o más variables de entorno no están definidas", DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT)
-
-DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-
-engine = create_engine(DATABASE_URL)
+engine = create_engine(settings.DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 def init_db():
     try:
         engine.connect()
-        print("Conexión exitosa a la base de datos")
+        print("Conectadooooooo")
     except Exception as error:
         print(f"Error al conectar a la base de datos: {error}")
+    
