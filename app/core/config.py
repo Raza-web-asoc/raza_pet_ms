914136ce517f8afc1138825c09b58882561602d1
Bach <daperezch@unal.edu.co>
2025-02-22 13:31:53 -0500

    coreconfig

--- app/core/config.py
+++ app/core/config.py
@@ -11,8 +11,8 @@ DB_PORT = os.getenv('DB_PORT')
 DB_URL = os.getenv('DB_URL')
 
 class Settings: 
-    # DATABASE_URL = f"mysql+pymysql://petdev:petdev@raza-pets-db:3306/petdev"
+    DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:3306/{DB_NAME}"
     # DATABASE_URL = f"mysql+mysqldb://root@/<dbname>?unix_socket=/cloudsql/<projectid>:<instancename>"
-    #DATABASE_URL = f"mysql+mysqldb://root@/pet-db?unix_socket=/cloudsql/proto3swarch:ins-pet-db"
-    DATABASE_URL = DB_URL
+    # DATABASE_URL = f"mysql+mysqldb://root@/pet-db?unix_socket=/cloudsql/proto3swarch:ins-pet-db"
+    # DATABASE_URL = DB_URL
 settings = Settings()
