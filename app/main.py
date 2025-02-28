f9e25a0103dddd4cb0437a0bf0dd99699ebca6f1
Juan Ovalle <jpovalleg@gmail.com>
2025-02-27 19:03:42 -0500

    Added prometheus

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ import sys
 import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from prometheus_fastapi_instrumentator import Instrumentator
 
 # Añadir el directorio raíz del proyecto al PYTHONPATH
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
@@ -21,10 +22,13 @@ app.add_middleware(
 
 init_db()
 
+instrumentator = Instrumentator(should_group_status_codes=False).instrument(app)
+instrumentator.expose(app, endpoint="/metrics")
+
 app.include_router(mascota.router, prefix="/mascotas", tags=["mascotas"])
 app.include_router(especie.router, prefix="/especies", tags=["especies"])
 app.include_router(raza.router, prefix="/razas", tags=["razas"])
 
 @app.get("/")
 def read_root():
-    return {"message": "¡Hola! FastAPI está corriendo correctamente."}
+    return {"message": "Welcome to the Pets API"}
